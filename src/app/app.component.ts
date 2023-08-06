085cb35d5d535fcc6be424d41ca76e0faa3dd2d0
Dariuslol <1026751@hr.nl>
2023-08-06 14:56:33 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as string_decoder from "string_decoder";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-project';
 }
+
